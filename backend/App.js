7895673fe19f31b09d22218836f1edd713f71dd3
Maimuna Ullah <mnu2234@rit.edu>
2026-02-24 21:36:46 -0500

    code to connect render and vercel

--- backend/App.js
+++ backend/App.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://todoapp-nine-psi-32.vercel.app"
+}));
 
 app.use("/api/todos", todoRoutes);
 /*
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     e.preventDefault();
     if (!newTodo.trim()) return;
     try {
-      const response = await axios.post("/api/todos", {text:newTodo});
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/todos`, {text:newTodo});
       setTodos([...todos, response.data]);
       setNewTodo("");
     }
@@ -29,7 +29,7 @@ function App() {
 
   const fetchTodos = async () => {
     try {
-      const response = await axios.get("/api/todos");
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/todos`);
       console.log("API response:", response.data);
       setTodos(response.data);
     }
@@ -49,7 +49,7 @@ function App() {
 
   const saveEdit = async (id) => {
     try {
-      const response = await axios.patch(`/api/todos/${id}`, {
+      const response = await axios.patch(`${import.meta.env.VITE_API_URL}/api/todos/${id}`, {
         text: editedText
       });
       setTodos(todos.map((todo) => (todo._id === id ? response.data 
@@ -63,7 +63,7 @@ function App() {
 
   const deleteTodo = async (id) => {
     try {
-      await axios.delete(`api/todos/${id}`);
+      await axios.delete(`${import.meta.env.VITE_API_URL}api/todos/${id}`);
       setTodos(todos.filter((todo) => todo._id !== id));
     }
     catch (error) {
@@ -74,7 +74,7 @@ function App() {
   const toggleTodo = async (id) => {
     try {
       const todo = todos.find((t) => t._id === id);
-      const response = await axios.patch(`/api/todos/${id}`, {
+      const response = await axios.patch(`${import.meta.env.VITE_API_URL}/api/todos/${id}`, {
         completed: !todo.completed
       });
       setTodos(todos.map((t) => t._id === id ? response.data : t));
